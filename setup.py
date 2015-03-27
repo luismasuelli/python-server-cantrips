f0ca0a0d828305059b7166dc23653da67d3cee00
luismasuelli <luismasuelli@hotmail.com>
2015-03-26 19:50:40 -0500

    server cantrips actualizados

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='python-server-cantrips',
-    version='0.0.1',
+    version='0.0.2',
     namespace_packages=['cantrips'],
     packages=find_packages(),
     url='https://github.com/luismasuelli/python-server-cantrips',
@@ -10,5 +10,5 @@ setup(
     author='Luis y Anita',
     author_email='luismasuelli@hotmail.com',
     description='Python library with small server patterns (i.e. not full protocols/managements, but just small patterns) to integrate a real-time server architecture',
-    install_requires=['python-cantrips>=0.6.5']
+    install_requires=['python-cantrips>=0.6.6']
 )
