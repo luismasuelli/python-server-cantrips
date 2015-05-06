d631c8386daa092f89c1740d1f72aee970999f48
luismasuelli <luismasuelli@hotmail.com>
2015-05-05 21:47:53 -0500

    eliminamos un argumento que ya no esperabamos

--- cantrips/protocol/tornado/websocket_server.py
+++ cantrips/protocol/tornado/websocket_server.py
@@ -3,7 +3,6 @@ try:
 except:
     raise ImportError("You need to install tornado for this to work (pip install tornado==4.0.2)")
 from cantrips.protocol.messaging import MessageProcessor
-from future.utils import istext
 
 
 class MessageHandler(WebSocketHandler, MessageProcessor):
@@ -36,4 +35,4 @@ class MessageHandler(WebSocketHandler, MessageProcessor):
         self._conn_made()
 
     def on_message(self, message):
-        self._conn_message(message, not istext(message))
+        self._conn_message(message)
