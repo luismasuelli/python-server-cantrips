b27d79d46f8f8de46aab6f52759f210b38e9f295
luismasuelli <luismasuelli@hotmail.com>
2015-12-05 19:15:11 -0500

    features varias nuevas (para timer)

--- cantrips/task/features.py
+++ cantrips/task/features.py
@@ -1,4 +1,3 @@
-from cantrips.types.exception import factory
 from cantrips.features import Feature
 
 
@@ -36,6 +35,7 @@ class TornadoFutureFeature(Feature):
         from tornado.concurrent import Future
         return Future
 
+    @classmethod
     def _import_error_message(cls):
         """
         Message error for tornado.concurrent.Future not found.
@@ -48,6 +48,7 @@ class TwistedDeferredFeature(Feature):
     Feature - twisted.internet.defer.Deferred
     """
 
+    @classmethod
     def _import_it(cls):
         """
         Imports Deferred from twisted.internet.defer.
@@ -55,8 +56,122 @@ class TwistedDeferredFeature(Feature):
         from twisted.internet.defer import Deferred
         return Deferred
 
+    @classmethod
     def _import_error_message(cls):
         """
         Message error for twisted.internet.defer.Deferred not found.
         """
         return "You need to install twisted framework for this to work (pip install twisted==14.0.2)"
+
+
+class ThreadedEventFeature(Feature):
+    """
+    Feature - threading.Event
+    """
+
+    @classmethod
+    def _import_it(cls):
+        """
+        Imports twisted.Event
+        """
+        from threading import Event
+        return Event
+
+    @classmethod
+    def _import_error_message(cls):
+        """
+        This message error should never be seen since threads are standard stuff.
+        """
+        return "Your standard library is corrupted. Module `threading` cannot be imported. Please reinstall" \
+               " your python distribution ASAP"
+
+
+class TornadoTimerFeature(Feature):
+    """
+    Feature - Timeouts for Tornado.
+    """
+
+    @classmethod
+    def _import_it(cls):
+        """
+        Imports stuff based on call_later and remove_callback.
+        Returns a pair of functions
+        """
+        from tornado.ioloop import IOLoop
+
+        def create_timeout(ioloop, seconds, callback):
+            return ioloop.call_later(seconds, callback)
+
+        def delete_timeout(ioloop, timeout):
+            try:
+                ioloop.remove_timeout(timeout)
+            except Exception as e:
+                pass
+
+        return create_timeout, delete_timeout
+
+    @classmethod
+    def _import_error_message(cls):
+        """
+        Message error for tornado.concurrent.IOLoop not found.
+        """
+        return "You need to install tornado for this to work (pip install tornado==4.0.2)"
+
+
+class TwistedTimerFeature(Feature):
+    """
+    Feature - Timeout for Twisted.
+    """
+
+    @classmethod
+    def _import_it(cls):
+        """
+        Imports stuff related to callLater and cancel.
+        Returns a pair of functions
+        """
+        from twisted.internet import reactor
+
+        def create_timeout(reactor, seconds, callback):
+            return reactor.callLater(seconds, callback)
+
+        def delete_timeout(timeout):
+            timeout.cancel()
+
+        return create_timeout, delete_timeout
+
+    @classmethod
+    def _import_error_message(cls):
+        """
+        Message error for twisted.internet.reactor not found.
+        """
+        return "You need to install twisted framework for this to work (pip install twisted==14.0.2)"
+
+
+class ThreadedTimerFeature(Feature):
+    """
+    Feature - Timeout for thread-based models.
+    """
+
+    @classmethod
+    def _import_it(cls):
+        """
+        Imports stuff related to threads.
+        Returns a pair of functions
+        """
+        from threading import Timer
+
+        def create_timeout(seconds, callback):
+            return Timer(seconds, callback)
+
+        def delete_timeout(timeout):
+            timeout.cancel()
+
+        return create_timeout, delete_timeout
+
+    @classmethod
+    def _import_error_message(cls):
+        """
+        This message error should never be seen since threads are standard stuff.
+        """
+        return "Your standard library is corrupted. Module `threading` cannot be imported. Please reinstall" \
+               " your python distribution ASAP"
