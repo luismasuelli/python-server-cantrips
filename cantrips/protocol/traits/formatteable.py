55439ad64898493af7d27c4a20f8bad2d6142972
luismasuelli <luismasuelli@hotmail.com>
2017-09-07 21:08:15 -0500

    Updating version, dependency and comment

--- cantrips/protocol/traits/formatteable.py
+++ cantrips/protocol/traits/formatteable.py
@@ -4,7 +4,7 @@ from cantrips.protocol.messaging.formats import FORMAT_STRING
 class IFormatteable(object):
     """
     Determines the current format to be used in identifiers. Allows to specify a current
-      format, and pick the appropiate format value for a specified CommandSpec.
+      format, and pick the appropriate format value for a specified CommandSpec.
     """
 
     COMMAND_FORMAT = FORMAT_STRING
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='python-server-cantrips',
-    version='0.0.5',
+    version='0.0.6',
     namespace_packages=['cantrips'],
     packages=find_packages(),
     url='https://github.com/luismasuelli/python-server-cantrips',
@@ -10,5 +10,5 @@ setup(
     author='Luis y Anita',
     author_email='luismasuelli@hotmail.com',
     description='Python library with small server patterns (i.e. not full protocols/managements, but just small patterns) to integrate a real-time server architecture',
-    install_requires=['python-cantrips>=0.7.1']
+    install_requires=['python-cantrips>=0.7.5', 'enum34>=1.0.4', 'six>=1.9.0', 'msgpack-python==0.4.8']
 )
